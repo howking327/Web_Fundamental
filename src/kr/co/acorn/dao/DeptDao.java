8e9faeb58fbb264d4e05a0ff4ca21b84fb0567cf
howking327 <howking327@gmail.com>
2019-12-17 18:06:33 +0900

    2019-12-17-v1

---
+++ src/kr/co/acorn/dao/DeptDao.java
@@ -0,0 +1,226 @@
+package kr.co.acorn.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import kr.co.acorn.dto.DeptDto;
+import kr.co.acorn.utill.ConnLocator;
+
+public class DeptDao {
+	
+	// 1. singleton pattern
+	//single이란 변수는 static 메서드에서 사용해야 하기 때문에 static 변수로 설정
+	private static DeptDao single;
+	//외부에서 객체를 생성할 수 없다
+	private DeptDao() {}
+	//외부에서 DeptDao 객체를 생성하기 위해서는 getInstance() 메서드를 이용해야 한다.
+	//따라서 이 메서드에서는 DeptDao 객체를 하나만 생성할 수 있게 코딩해야 한다.
+	public static DeptDao getInstance() {
+		if (single == null) {
+			single = new DeptDao();
+		}
+		return single;
+	}
+
+	public boolean insert(DeptDto dto) {
+		boolean isSuccess = false;
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		try {
+			
+			con = ConnLocator.getConnection();
+
+			StringBuffer sql = new StringBuffer();
+			sql.append("INSERT INTO dept(deptno, dname, loc) VALUES(?, ?, ?)"); 
+			
+			
+			pstmt = con.prepareStatement(sql.toString());
+			
+			int index = 0;
+			pstmt.setInt(++index, dto.getNo());
+			pstmt.setString(++index, dto.getName());
+			pstmt.setString(++index, dto.getLoc());
+
+			
+			pstmt.executeUpdate();
+			
+			isSuccess = true;
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if(pstmt != null) pstmt.close();
+				if(con != null) con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return isSuccess;
+	}
+
+	public boolean update(DeptDto dto) {
+		boolean isSuccess = false;
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		try {
+			con = ConnLocator.getConnection();
+			
+			StringBuffer sql = new StringBuffer();
+			sql.append("UPDATE dept ");
+			sql.append("SET dname = ?, loc = ? ");
+			sql.append("WHERE deptno = ?");
+			
+			pstmt = con.prepareStatement(sql.toString());
+			
+			int index = 0;
+			pstmt.setString(++index, dto.getName());
+			pstmt.setString(++index, dto.getLoc());
+			pstmt.setInt(++index, dto.getNo());
+			
+			pstmt.executeUpdate();
+			
+			isSuccess = true;
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if(pstmt != null) pstmt.close();
+				if(con != null) con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return isSuccess;
+	}
+
+	public boolean delete(int deptNo) {
+		boolean isSuccess = false;
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		try {
+			con = ConnLocator.getConnection();
+			
+			StringBuffer sql = new StringBuffer();
+			sql.append("DELETE FROM dept WHERE deptno = ?");
+			
+			pstmt = con.prepareStatement(sql.toString());
+			
+			int index = 0;
+			pstmt.setInt(++index, deptNo);
+			
+			pstmt.executeUpdate();
+			
+			isSuccess = true;
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+				try {
+					if(pstmt != null) pstmt.close();
+					if(con != null) con.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		}
+		return isSuccess;
+	}
+
+	public ArrayList<DeptDto> select() {
+		ArrayList<DeptDto> list = new ArrayList<DeptDto>();
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			con = ConnLocator.getConnection();
+			StringBuffer sql = new StringBuffer();
+			sql.append("SELECT deptno, dname, loc ");
+			sql.append("FROM dept ");
+			sql.append("ORDER BY deptno");
+			
+			pstmt = con.prepareStatement(sql.toString());
+			
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()){
+				int index = 0;
+				int deptno = rs.getInt(++index);
+				String dname = rs.getString(++index);
+				String loc = rs.getString(++index);
+				list.add(new DeptDto(deptno,dname,loc));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+				try {
+					if(rs != null) rs.close();
+					if(pstmt != null) pstmt.close();
+					if(con != null) con.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		}
+		return list;
+	}
+
+	public DeptDto select(int deptNo) {
+		DeptDto dto = null;
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			con = ConnLocator.getConnection();
+			StringBuffer sql = new StringBuffer();
+			sql.append("SELECT deptno, dname, loc ");
+			sql.append("FROM dept ");
+			sql.append("WHERE deptno = ?");
+			
+			pstmt = con.prepareStatement(sql.toString());
+			
+			int index = 0;
+			pstmt.setInt(++index, deptNo);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()){
+				index = 0;
+				int deptno = rs.getInt(++index);
+				String dname = rs.getString(++index);
+				String loc = rs.getString(++index);
+				dto = new DeptDto(deptno,dname,loc);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+				try {
+					if(rs != null) rs.close();
+					if(pstmt != null) pstmt.close();
+					if(con != null) con.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		}
+		return dto;
+	}
+}
